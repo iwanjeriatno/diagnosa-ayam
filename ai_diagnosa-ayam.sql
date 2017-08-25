3ec5491f66159b29316bd0d7f8acabff6a9b3a03
iwanjeriatno <jeriat01@gmail.com>
2017-08-25 13:35:42 +0800

    db

--- ai_diagnosa-ayam.sql
+++ ai_diagnosa-ayam.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: 24 Agu 2017 pada 07.53
+-- Generation Time: 25 Agu 2017 pada 14.34
 -- Versi Server: 5.7.19-0ubuntu0.16.04.1
 -- PHP Version: 7.0.22-0ubuntu0.16.04.1
 
@@ -46,83 +46,76 @@ CREATE TABLE `detail-user` (
 CREATE TABLE `gejala` (
   `id` int(11) NOT NULL,
   `kd_gejala` varchar(10) NOT NULL,
-  `nama_gejala` varchar(255) NOT NULL,
-  `gejala_awal_ya` varchar(10) DEFAULT NULL,
-  `gejala_awal_tidak` varchar(10) DEFAULT NULL
+  `nama_gejala` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `gejala`
 --
 
-INSERT INTO `gejala` (`id`, `kd_gejala`, `nama_gejala`, `gejala_awal_ya`, `gejala_awal_tidak`) VALUES
-(9, 'G004', 'Sulit bernafas mengap mengap', NULL, NULL),
-(10, 'G005', 'Depresi', NULL, NULL),
-(11, 'G006', 'Rongga hidung dan mulut berisi cairan lendir', NULL, NULL),
-(12, 'G007', 'Produksi telur menurun', NULL, NULL),
-(13, 'G008', 'Diare berwarna hijau', NULL, NULL),
-(14, 'G009', 'Pembengkakan dibagian kepala dan leher', NULL, NULL),
-(15, 'G010', 'Muka, jengger, dan piar pucat', NULL, NULL),
-(16, 'G011', 'Bulu kotor dan kusam', NULL, NULL),
-(17, 'G012', 'Ngorok', NULL, NULL),
-(18, 'G013', 'Pertumbuhan lambat', NULL, NULL),
-(19, 'G014', 'Mata selalu basah', NULL, NULL),
-(20, 'G015', 'Batuk', NULL, NULL),
-(21, 'G016', 'Mengantuk dan menggigil', NULL, NULL),
-(22, 'G017', 'Pendarahan pada otot dan paha', NULL, NULL),
-(23, 'G018', 'Bersin bersin', NULL, NULL),
-(24, 'G019', 'Dehidrasi', NULL, NULL),
-(25, 'G020', 'Sayap terkulai kulai', NULL, NULL),
-(26, 'G021', 'Pembengkakan rongga mata dan berdarah', NULL, NULL),
-(27, 'G022', 'Bengkak pada paha', NULL, NULL),
-(28, 'G023', 'Nafas cepat', NULL, NULL),
-(29, 'G024', 'Kelumpuhan', NULL, NULL),
-(30, 'G025', 'Keluar lendir dengan bau pada hidung', NULL, NULL),
-(31, 'G026', 'Menggeleng gelengkan kepalanya', NULL, NULL),
-(32, 'G027', 'Terjadi pembengkakan pada mata dan muka', NULL, NULL),
-(33, 'G028', 'Luka keropeng dibagian kulit', NULL, NULL),
-(34, 'G029', 'Tingkat produktivitas menurun', NULL, NULL),
-(35, 'G030', 'Kurus', NULL, NULL),
-(36, 'G031', 'Muka bengkak', NULL, NULL),
-(37, 'G032', 'Kualitas telur buruk', NULL, NULL),
-(38, 'G033', 'Lesuh', NULL, NULL),
-(39, 'G034', 'Bulu jarang', NULL, NULL),
-(40, 'G035', 'Mengibaskan kepalanya secara kuat', NULL, NULL),
-(41, 'G036', 'Kepala dijulurkan kedepan', NULL, NULL),
-(43, 'G037', 'Mata buta', '_', '_'),
-(44, 'G038', 'Kaki memborok', '_', '_'),
-(46, 'G001', 'Diare berwarna putih', '_', '_'),
-(47, 'G002', 'Mengantuk dan lemah', '_', '_'),
-(48, 'G003', 'Nafsu makan berkurang', '_', '_'),
-(49, 'G039', 'Tidak ada gejala', '_', '_');
+INSERT INTO `gejala` (`id`, `kd_gejala`, `nama_gejala`) VALUES
+(9, 'G004', 'Sulit bernafas mengap mengap'),
+(10, 'G005', 'Depresi'),
+(11, 'G006', 'Rongga hidung dan mulut berisi cairan lendir'),
+(12, 'G007', 'Produksi telur menurun'),
+(13, 'G008', 'Diare berwarna hijau'),
+(14, 'G009', 'Pembengkakan dibagian kepala dan leher'),
+(15, 'G010', 'Muka, jengger, dan piar pucat'),
+(16, 'G011', 'Bulu kotor dan kusam'),
+(17, 'G012', 'Ngorok'),
+(18, 'G013', 'Pertumbuhan lambat'),
+(19, 'G014', 'Mata selalu basah'),
+(20, 'G015', 'Batuk'),
+(21, 'G016', 'Mengantuk dan menggigil'),
+(22, 'G017', 'Pendarahan pada otot dan paha'),
+(23, 'G018', 'Bersin bersin'),
+(24, 'G019', 'Dehidrasi'),
+(25, 'G020', 'Sayap terkulai kulai'),
+(26, 'G021', 'Pembengkakan rongga mata dan berdarah'),
+(27, 'G022', 'Bengkak pada paha'),
+(28, 'G023', 'Nafas cepat'),
+(29, 'G024', 'Kelumpuhan'),
+(30, 'G025', 'Keluar lendir dengan bau pada hidung'),
+(31, 'G026', 'Menggeleng gelengkan kepalanya'),
+(32, 'G027', 'Terjadi pembengkakan pada mata dan muka'),
+(33, 'G028', 'Luka keropeng dibagian kulit'),
+(34, 'G029', 'Tingkat produktivitas menurun'),
+(35, 'G030', 'Kurus'),
+(36, 'G031', 'Muka bengkak'),
+(37, 'G032', 'Kualitas telur buruk'),
+(38, 'G033', 'Lesuh'),
+(39, 'G034', 'Bulu jarang'),
+(40, 'G035', 'Mengibaskan kepalanya secara kuat'),
+(41, 'G036', 'Kepala dijulurkan kedepan'),
+(43, 'G037', 'Mata buta'),
+(44, 'G038', 'Kaki memborok'),
+(46, 'G001', 'Diare berwarna putih'),
+(47, 'G002', 'Mengantuk dan lemah'),
+(48, 'G003', 'Nafsu makan berkurang'),
+(49, 'G039', 'Tidak ada gejala');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `gejala_solusi`
+-- Struktur dari tabel `hasil_identifikasi`
 --
 
-CREATE TABLE `gejala_solusi` (
-  `id` int(11) NOT NULL,
-  `kd_gejala` varchar(10) NOT NULL,
-  `kd_solusi` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `hasil_diagnosa`
---
-
-CREATE TABLE `hasil_diagnosa` (
+CREATE TABLE `hasil_identifikasi` (
   `id_diagnosa` int(11) NOT NULL,
-  `kd_penyakit` varchar(5) NOT NULL,
+  `tanggal_identifikasi` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `hasil_identifikasi` varchar(255) NOT NULL,
   `pengobatan` varchar(200) NOT NULL,
-  `nama` varchar(100) NOT NULL,
-  `jenis_kelamin` varchar(20) NOT NULL,
-  `alamat` text NOT NULL
+  `id_user` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `hasil_identifikasi`
+--
+
+INSERT INTO `hasil_identifikasi` (`id_diagnosa`, `tanggal_identifikasi`, `hasil_identifikasi`, `pengobatan`, `id_user`) VALUES
+(7, '2017-08-25 01:56:59', 'Mengalami Penyakit Berak kapur - Pullorium disease', 'Menyuntikkan antibiotik seperti furozolidon, coccilin, neo terramycin, tetra atau mycomas  di dada ayam.  Penulis lain menyebutkan pengobatan dapat dilakukan dengan menggunakan preparat sulfonamide.', 2),
+(8, '2017-08-25 03:16:30', 'Mengalami Penyakit Kolero unggas - Fowl cholera', 'Beri obat yg mengandumg Tetrasiklin, Oksitetrasiklin, Sulfadiazine, Amoksisilin, Enrofloksasin', 5),
+(9, '2017-08-25 05:07:24', 'Mengalami Penyakit Berak kapur - Pullorium disease', 'Menyuntikkan antibiotik seperti furozolidon, coccilin, neo terramycin, tetra atau mycomas  di dada ayam.  Penulis lain menyebutkan pengobatan dapat dilakukan dengan menggunakan preparat sulfonamide.', 5);
 
 -- --------------------------------------------------------
 
@@ -225,18 +218,6 @@ INSERT INTO `penyakit_gejala` (`id`, `kd_penyakit`, `kd_gejala`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pertanyaan`
---
-
-CREATE TABLE `pertanyaan` (
-  `kd_pertanyaan` int(11) NOT NULL,
-  `pertanyaan` varchar(255) NOT NULL,
-  `kd_gejala` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Struktur dari tabel `rule`
 --
 
@@ -302,6 +283,7 @@ INSERT INTO `rule` (`id`, `kd_gejala`, `jika_ya`, `jika_tidak`) VALUES
 CREATE TABLE `solusi` (
   `id` int(11) NOT NULL,
   `kd_solusi` varchar(10) NOT NULL,
+  `kd_penyakit` varchar(10) NOT NULL,
   `solusi` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -309,44 +291,19 @@ CREATE TABLE `solusi` (
 -- Dumping data untuk tabel `solusi`
 --
 
-INSERT INTO `solusi` (`id`, `kd_solusi`, `solusi`) VALUES
-(1, 'S0000001', 'dfdsfdszfdsgzfvdz'),
-(2, 'S0000002', 'zfgdfgsdrfg');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `tmp_diagnosa`
---
-
-CREATE TABLE `tmp_diagnosa` (
-  `username` varchar(10) NOT NULL,
-  `kode_penyakit` varchar(5) NOT NULL,
-  `kode_gejala` varchar(5) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `tmp_gejala`
---
-
-CREATE TABLE `tmp_gejala` (
-  `username` varchar(10) NOT NULL,
-  `kode_gejala` varchar(5) NOT NULL,
-  `status` enum('1','0') NOT NULL DEFAULT '1'
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `tmp_penyakit`
---
-
-CREATE TABLE `tmp_penyakit` (
-  `username` varchar(10) NOT NULL,
-  `kode_penyakit` varchar(5) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+INSERT INTO `solusi` (`id`, `kd_solusi`, `kd_penyakit`, `solusi`) VALUES
+(1, 'S001', 'P001', 'Menyuntikkan antibiotik seperti furozolidon, coccilin, neo terramycin, tetra atau mycomas  di dada ayam.  Penulis lain menyebutkan pengobatan dapat dilakukan dengan menggunakan preparat sulfonamide.'),
+(2, 'S002', 'P002', 'Beri obat yg mengandumg Tetrasiklin, Oksitetrasiklin, Sulfadiazine, Amoksisilin, Enrofloksasin'),
+(3, 'S003', 'P003', 'Antibiotik selama 3-5 hari untukl mencegah adanya penyakit sekunder oleh bakteri, multivitamin untuk meningkatkan kondisi tubuh dan proses penyembuhan, penyediaan ransum dan air minum segar.  Revaksinasi (vaksinasi ulang) jika diperlukan terutama jika umur pemeliharaan masih memungkinkan.'),
+(4, 'S004', 'P004', 'Dengan tetra chlor, furox, neo sulfa, nocci, furol tetra, ebs 3 atau bisa di suntikkan terramycin '),
+(5, 'S005', 'P005', 'Dapat diberikan baytrit 10% peroral, mycomas dengan dosis 0.5 ml/L air minum, tetraclorin secara	 oral atau bacytracyn yang diberikan pada air minum.'),
+(6, 'S006', 'P006', 'Pemberian air gula (5-8%), parasetamol, multivitamin untuk meningkatkan kondisi tubuh dan proses penyembuhan, pemberian pemanas untu anak ayam, antibiotik (3-5 hari) untuk mencegah infeksi sekunder.  Pilih antibiotik yang tidak mempengaruhi kerja ginjal'),
+(7, 'S007', 'P008', 'Paratipus :  dapat di berikan furazolidon, gentamisin, spektinomisin dan sulfametazin.'),
+(8, 'S008', 'P007', 'Berikan obat coridrol, vita chloricol, cory sp, erynosil kapsul, dan tetra strep.'),
+(9, 'S009', 'P009', 'Musnahkan ayam yang terkena batuk darah karena sifat peyakit ini cepat menular.penanganannya dengan cara pemberian vaksinasi LT dengan cara meneteskan pada mata anak ayam yang berumur 14 hari'),
+(10, 'S010', 'P011', 'Dapat di berikan preparat sulfat, misalnya sulfadimethoxine atau sulfathiazole.'),
+(11, 'S011', 'P012', 'Penyakit ini secara tradisional dapat disembuhkan dengan mengolesi minyak tanah pada bagian yang sakit secara rutin. Hal ini bisa di lakukan pagi, siang dan sore atau bisa juga pagi sore saja, dalam waktu 3-4 hari insyaAllah sembuh.'),
+(12, 'S012', 'P010', 'Ayam yg erkena mereks sebaiknya di di musnahkan agar tidak menular kea yam sehat lain. Untk encegah penyakit mereks sebaiknya ayam di vaksin mereks dr umur 1 hari dengan vaksin cryomarex HVT.');
 
 -- --------------------------------------------------------
 
@@ -358,7 +315,7 @@ CREATE TABLE `users` (
   `id_user` int(11) NOT NULL,
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `role` enum('pakar','pengguna') NOT NULL
+  `role` enum('pakar','pengguna') NOT NULL DEFAULT 'pengguna'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -367,7 +324,8 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id_user`, `username`, `password`, `role`) VALUES
 (1, 'pakar', '87b7cf2448de01f22b0c016b272f2ec0', 'pakar'),
-(2, 'iqbal', 'eedae20fc3c7a6e9c5b1102098771c70', 'pengguna');
+(2, 'iqbal', 'eedae20fc3c7a6e9c5b1102098771c70', 'pengguna'),
+(5, 'uppa', 'cf1fe2491216626c99b4d428eddae044', 'pengguna');
 
 --
 -- Indexes for dumped tables
@@ -385,23 +343,14 @@ ALTER TABLE `detail-user`
 --
 ALTER TABLE `gejala`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `kd_gejala` (`kd_gejala`),
-  ADD KEY `gejala_awal_ya` (`gejala_awal_ya`),
-  ADD KEY `gejala_awal_tidak` (`gejala_awal_tidak`);
+  ADD UNIQUE KEY `kd_gejala` (`kd_gejala`);
 
 --
--- Indexes for table `gejala_solusi`
+-- Indexes for table `hasil_identifikasi`
 --
-ALTER TABLE `gejala_solusi`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `kd_gejala` (`kd_gejala`),
-  ADD KEY `kd_solusi` (`kd_solusi`);
-
---
--- Indexes for table `hasil_diagnosa`
---
-ALTER TABLE `hasil_diagnosa`
-  ADD PRIMARY KEY (`id_diagnosa`);
+ALTER TABLE `hasil_identifikasi`
+  ADD PRIMARY KEY (`id_diagnosa`),
+  ADD KEY `id_user` (`id_user`);
 
 --
 -- Indexes for table `penyakit`
@@ -419,13 +368,6 @@ ALTER TABLE `penyakit_gejala`
   ADD KEY `kd_gejala` (`kd_gejala`);
 
 --
--- Indexes for table `pertanyaan`
---
-ALTER TABLE `pertanyaan`
-  ADD PRIMARY KEY (`kd_pertanyaan`),
-  ADD KEY `kd_gejala` (`kd_gejala`);
-
---
 -- Indexes for table `rule`
 --
 ALTER TABLE `rule`
@@ -439,7 +381,8 @@ ALTER TABLE `rule`
 --
 ALTER TABLE `solusi`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `kd_solusi` (`kd_solusi`);
+  ADD UNIQUE KEY `kd_solusi` (`kd_solusi`),
+  ADD KEY `kd_penyakit` (`kd_penyakit`);
 
 --
 -- Indexes for table `users`
@@ -462,15 +405,10 @@ ALTER TABLE `detail-user`
 ALTER TABLE `gejala`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 --
--- AUTO_INCREMENT for table `gejala_solusi`
+-- AUTO_INCREMENT for table `hasil_identifikasi`
 --
-ALTER TABLE `gejala_solusi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `hasil_diagnosa`
---
-ALTER TABLE `hasil_diagnosa`
-  MODIFY `id_diagnosa` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `hasil_identifikasi`
+  MODIFY `id_diagnosa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `penyakit`
 --
@@ -482,11 +420,6 @@ ALTER TABLE `penyakit`
 ALTER TABLE `penyakit_gejala`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
 --
--- AUTO_INCREMENT for table `pertanyaan`
---
-ALTER TABLE `pertanyaan`
-  MODIFY `kd_pertanyaan` int(11) NOT NULL AUTO_INCREMENT;
---
 -- AUTO_INCREMENT for table `rule`
 --
 ALTER TABLE `rule`
@@ -495,12 +428,12 @@ ALTER TABLE `rule`
 -- AUTO_INCREMENT for table `solusi`
 --
 ALTER TABLE `solusi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;COMMIT;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
